8bbfca56e0cc427e852986d6f911f491748e3325
Adarsh Malviya <56603301+adarshmalviya@users.noreply.github.com>
2022-02-06 12:35:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(post)
 app.use(user)
 
 if (process.env.NODE_ENV == "production") {
-    app.use(express.static('clienc/build'))
+    app.use(express.static('client/build'))
     const path = require('path')
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
